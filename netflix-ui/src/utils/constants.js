cadea52df5172a3fa4f170b9c92cb522d32d56c2
Nidhi Prakash <np562019@gmail.com>
2024-02-05 22:03:47 +0530

    added env file + added credentials for firebase

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const TMDB_BASE_URL = process.env.REACT_APP_TMDB_BASE_URL;
